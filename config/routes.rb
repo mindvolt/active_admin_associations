e7deadae84d6e4394708a594e850760c4246dc39
redhotcode <shaneify@gmail.com>
2014-03-19 23:11:04 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8 
 if Rails.application.config.activeadmin_associations.autocomplete_models
   models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
   Rails.application.routes.draw do
