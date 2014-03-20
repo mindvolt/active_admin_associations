cbe75644cc439b778bde8d0c8f731e521f8f0083
Shane Kamar <shanekamar@mac.com>
2014-03-19 23:33:32 -0500

    Did some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ if Rails.application.config.activeadmin_associations.autocomplete_models
   models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
   Rails.application.routes.draw do
     match '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/,
-      :defaults => { :format => 'json' }, via: [:get, :post]
+      :defaults => { :format => 'json' }
   end
 end
